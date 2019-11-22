6ca4be06d748d4b96789fb4ae43f8af852b51f39
christophsaile <54807480+christophsaile@users.noreply.github.com>
2019-11-22 13:50:14 +0100

    first commit

--- src/components/home.js
+++ src/components/home.js
@@ -68,29 +68,6 @@ class Home extends Component {
           <Helmet>
             <title>Today I learned | Home</title>
           </Helmet>
-          <h1 className="centered">Today I learned</h1>
-
-          <Card>
-            <h2>What is this?</h2>
-            <p>
-              Web development and being a "programmer" often feels overwhelming.
-              The amount of information out there is very intimidating.
-              Especially when you're entering the field it's very easy to feel
-              lost and stuck.
-            </p>
-            <p>
-              Writing down learnings helps to fight this feeling. With this
-              project you can easily start building a central place to see the
-              progress on your learning journey.
-            </p>
-            <p>
-              And maybe (just maybe)... you want to share your learnings, too,
-              because what you'll learn over time is that nobody knows
-              everything and that every learning from your perspective can be
-              completely new to someone else.
-            </p>
-          </Card>
-
           <h2 className="centered">Learnings</h2>
           <ul className="o-grid">
             {Object.entries(categories).map(
